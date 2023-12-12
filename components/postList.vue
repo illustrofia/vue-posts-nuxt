0119697a0fe7df6e7c7f549a0a8849d449cccdbd
Luca Airinei <illustrofia@gmail.com>
2023-12-12 22:36:10 +0100

    feat(post list): add move post feature

--- components/postList.vue
+++ components/postList.vue
@@ -9,6 +9,16 @@
     <ul v-if="!pending && !error" class="list-none flex flex-col gap-4">
       <li v-for="post in posts" :key="post.id" class="flex justify-between py-4 px-2 w-64">
         <h3>Post {{ post.id }}</h3>
+        <div class="flex flex-col gap-1">
+          <span v-if="posts.indexOf(post) !== 0" @click="() => movePost(post.id, 'up')"
+            >move up</span
+          >
+          <span
+            v-if="posts.indexOf(post) !== posts.length - 1"
+            @click="() => movePost(post.id, 'down')"
+            >move down</span
+          >
+        </div>
       </li>
     </ul>
   </div>
@@ -22,6 +32,8 @@ interface Post {
   body: string
 }
 
+const posts = ref<Post[]>([])
+
 // Fetch posts and store them in a reactive state
 const { data, pending, error } = useLazyFetch<Post[]>(
   'https://jsonplaceholder.typicode.com/posts',
@@ -30,10 +42,17 @@ const { data, pending, error } = useLazyFetch<Post[]>(
     transform: (data) => data.slice(0, 5)
   }
 )
-
-const posts = ref<Post[]>([])
-
 watch(data, (value) => {
   posts.value = value ?? []
 })
+
+const movePost = (id: number, direction: 'up' | 'down') => {
+  const index = posts.value.findIndex((post) => post.id === id)
+  if (index === -1) {
+    return
+  }
+  const post = posts.value[index]
+  posts.value.splice(index, 1)
+  posts.value.splice(index + (direction === 'up' ? -1 : 1), 0, post)
+}
 </script>
