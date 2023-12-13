b168911881690562ace296c492a63fb6ab8f1f2a
Luca Airinei <illustrofia@gmail.com>
2023-12-13 16:24:42 +0100

    feat(post list history): extract post history action component

---
+++ components/PostHistoryAction.vue
@@ -0,0 +1,33 @@
+<template>
+  <li
+    :class="{
+      'flex flex-col sm:flex-row items-center gap-4 justify-between p-3 border border-slate-100 bg-white transition-all': true,
+      'rounded-t': isFirst,
+      'rounded-b': isLast
+    }"
+  >
+    <span class="text-xs sm:text-sm text-gray-700">
+      Moved post {{ postAction.movedPostId }} from index
+      {{ postAction.from }} to index {{ postAction.to }}
+    </span>
+    <button
+      class="bg-green-300 hover:bg-green-200 text-green-900 hover:text-green-800 border border-green-400 hover:border-green-300 py-1 sm:py-2 px-2 sm:px-4 text-xs sm:text-sm font-semibold rounded transition-all drop-shadow-sm hover:drop-shadow-md active:bg-green-400 active:text-green-900 active:border-green-400 shrink-0"
+      @click="$emit('time-travel', postAction.timestamp)"
+    >
+      Time travel
+    </button>
+  </li>
+</template>
+
+<script setup lang="ts">
+import type { PostAction } from '~/@types'
+
+defineProps<{
+  postAction: PostAction
+  isFirst: boolean
+  isLast: boolean
+  onTimeTravel: (timestamp: string) => void
+}>()
+
+defineEmits(['time-travel'])
+</script>
--- components/PostListHistory.vue
+++ components/PostListHistory.vue
@@ -2,7 +2,7 @@
   <div
     class="flex flex-col drop-shadow-md rounded border border-slate-100 bg-white md:min-w-[27.5rem]"
   >
-    <div class="p-5">
+    <div class="p-4">
       <h2 class="font-medium text-lg">List of actions commited</h2>
     </div>
     <div class="bg-slate-100 p-4 h-full">
@@ -16,28 +16,17 @@
         name="activity-history"
       >
         <!-- TODO: animate list item leave -->
-        <li
+        <PostHistoryAction
           v-for="postAction in store.postActionHistory"
-          :key="JSON.stringify(postAction)"
-          :class="{
-            'flex flex-col sm:flex-row items-center gap-4 justify-between p-3 border border-slate-100 bg-white transition-all': true,
-            'rounded-t': store.postActionHistory.indexOf(postAction) === 0,
-            'rounded-b':
-              store.postActionHistory.indexOf(postAction) ===
-              store.postActionHistory.length - 1
-          }"
-        >
-          <span class="text-sm text-gray-700">
-            Moved post {{ postAction.movedPostId }} from index
-            {{ postAction.from }} to index {{ postAction.to }}
-          </span>
-          <button
-            class="bg-green-300 hover:bg-green-200 text-green-900 hover:text-green-800 border border-green-400 hover:border-green-300 py-2 px-4 text-sm font-semibold rounded transition-all drop-shadow-sm hover:drop-shadow-md active:bg-green-400 active:text-green-900 active:border-green-400 shrink-0"
-            @click="() => store.revertToPostIdListOrder(postAction.timestamp)"
-          >
-            Time travel
-          </button>
-        </li>
+          :key="postAction.timestamp"
+          :post-action="postAction"
+          :is-first="store.postActionHistory.indexOf(postAction) === 0"
+          :is-last="
+            store.postActionHistory.indexOf(postAction) ===
+            store.postActionHistory.length - 1
+          "
+          @time-travel="store.revertToPostIdListOrder"
+        />
       </TransitionGroup>
     </div>
   </div>
