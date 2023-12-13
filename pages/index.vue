b5df288ff260fbb2df8d3336b1ef2f01b2f130df
Luca Airinei <illustrofia@gmail.com>
2023-12-13 01:49:58 +0100

    chore(fonts): add Inter font

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen w-screen">
+  <div class="min-h-screen w-screen font-body">
     <div class="flex justify-between p-10">
       <PostList />
       <PostListHistory />
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     './error.vue'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        body: ['Inter', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
